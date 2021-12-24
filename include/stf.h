79426ccc0fe5348893297a151f6f111347202f58
Ayagaki-muti <1450057425@qq.com>
2021-12-24 01:38:40 -0800

    add interface specifications of time stamp

--- include/stf.h
+++ include/stf.h
@@ -1,3 +1,6 @@
+/**
+ * @brief STF国标接口
+ */
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) ||          \
@@ -23,25 +26,8 @@
 extern "C" {
 #endif
 // 时间戳服务接口常量定义
-
 typedef unsigned char SGD_UINT8;
 typedef unsigned int SGD_UINT32;
-
-/**
- * @brief
- * @param phTSHandle
- * @return
- */
-SGD_UINT32 STF_InitEnvironment(void** phTSHandle);
-
-/**
- * @brief
- * @param hTSHandle
- * @return
- */
-SGD_UINT32 STF_ClearEnvironment(void* hTSHandle);
-
-
 
 // 返回值与错误码定义
 #define STF_TS_OK 0
@@ -60,12 +46,129 @@ SGD_UINT32 STF_ClearEnvironment(void* hTSHandle);
 #define STF_TS_ADDINFO_NOT_AVAILBLE 0x0400000C
 #define STF_TS_SYSTEM_FAILURE 0x0400000E
 
+/**
+ * @brief 建立时间戳环境句柄
+ * @param phTSHandle[out]: 时间戳环境句柄指针
+ * @return 0:成功; 其他:失败
+ */
+SGD_UINT32 STF_InitEnvironment(void **phTSHandle);
 
+/**
+ * @brief 清除时间戳环境句柄
+ * @param hTSHandle[in]:时间戳环境句柄
+ * @return 0:成功; 其他:失败
+ */
+SGD_UINT32 STF_ClearEnvironment(void *hTSHandle);
 
+/**
+ * @brief 用指定算法对时间戳请求信息indata进行密码杂凑运算，生成时间戳请求包
+ * @param hTSHandle[in]: 时间戳环境句柄
+ * @param pucInData[in]: 需要加盖时间戳的用户信息
+ * @param uiInDataLength[in]: 用户信息的长度
+ * @param uiReqType[in]: 请求的时间戳服务类型
+ * @param pucTSExt[in]: 时间戳请求包的其他扩展，DER编码格式
+ * @param uiHashAlgID[in]: 密码杂凑算法标识
+ * @param pucTSRequest[out]: 时间戳请求
+ * @param puiTSRequestLength[in,out]: 时间戳请求的长度
+ * @return 0:成功; 其他:失败
+ * @note uiReqType:
+ * 0代表时间戳响应应该包含时间戳服务器的证书，1代表时间戳响应不包含时间戳服务器的证书
+ * puiTSRequestLength[in,out]:
+ * 入口值为指定的用于存放时间戳请求的字符数组的最大长度，出口值为时间戳请求的实际长度
+ */
+SGD_UINT32 STF_CreateTSRequest(void *hTSHandle, SGD_UINT8 *pucInData,
+                               SGD_UINT32 uiInDataLength, SGD_UINT32 uiReqType,
+                               SGD_UINT8 *pucTSExt, SGD_UINT32 uiHashAlgID,
+                               SGD_UINT8 *pucTSRequest,
+                               SGD_UINT32 *puiTSRequestLength);
 
+/**
+ * @brief 根据时间戳请求包生成时间戳响应包
+ * @param hTSHandle[in]: 时间戳环境句柄
+ * @param pucTSRequest[in]: 时间戳请求
+ * @param uiTSRequestLength[in]: 时间戳请求的长度
+ * @param uiSignatureAlgID[in]: 签名算法标识
+ * @param pucTSResponse[out]: 时间戳响应
+ * @param puiTSResponseLength[in,out]: 时间戳响应的长度
+ * @return 0:成功; 其他:失败
+ * @note puiTSResponseLength[in,out]:
+ * 入口值为指定的用于存放时间戳的字符数组的最大长度，出口值为时间戳的实际长度
+ */
+SGD_UINT32 STF_CreateTSReponse(void *hTSHandle, SGD_UINT8 *pucTSRequest,
+                               SGD_UINT32 uiTSRequestLength,
+                               SGD_UINT32 uiSignatureAlgID,
+                               SGD_UINT8 *pucTSResponse,
+                               SGD_UINT32 *puiTSResponseLength);
 
+/**
+ * @brief 验证时间戳响应是否有效
+ * @param hTSHandle[in]: 时间戳环境句柄
+ * @param pucTSResponse[in]: 获取的时间戳响应
+ * @param uiTSResponseLength[in]: 时间戳响应的长度
+ * @param uiHashAlgID[in]: 密码杂凑算法标识
+ * @param uiSignatureAlgID[in]: 签名算法标识
+ * @param pucTSCert[in]: TSA的证书，DER编码格式
+ * @param uiTSCertLength[in]: TSA证书的长度
+ * @return 0:成功; 其他:失败
+ * @note
+ * 该函数验证时间戳响应是否有效。对于不包含时间戳服务器证书的响应，需要指定时间戳服务器的证书才能进行验证；
+ * 对于包含时间戳服务器证书的响应，可以把入口的证书参数置为空，使用响应中自带的证书进行验证，否则将使用指定的证书进行验证，即指定的证书优先于自带的证书。
+ */
+SGD_UINT32 STF_VerifyTSValidity(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                                SGD_UINT32 uiTSResponseLength,
+                                SGD_UINT32 uiHashAlgID,
+                                SGD_UINT32 uiSignatureAlgID,
+                                SGD_UINT8 *pucTSCert,
+                                SGD_UINT32 uiTSCertLength);
+/**
+ * @brief 获取时间戳的主要信息。
+ * @param hTSHandle[in]: 时间戳环境句柄
+ * @param pucTSResponse[in]: 获取的时间戳响应
+ * @param uiTSResponseLength[in]: 时间戳响应的长度
+ * @param pucIssuerName[out]: TSA的通用名
+ * @param puiIssuerNameLength[in,out]: TSA通用名的长度
+ * @param pucTime[out]: 时间戳标注的时间值
+ * @param puiTimeLength[in,out]: 时间戳标注的时间值长度
+ * @return 0:成功; 其他:失败
+ * @note 该函数解析时间戳的主要信息，包括TSA的通用名和时间戳的签发时间。
+ * puiIssuerNameLength[in,out]:
+ * 入口值为指定的用于存放签发者名称的字符数组的最大长度，出口值为签发者名称的实际长度
+ * puiTimeLength[in,out]:
+ * 入口值为指定的用于存放时间戳标注时间值的字符数组的最大长度，出口值为时间戳标注的时间值的实际长度
+ */
+SGD_UINT32 STF_GetTSInfo(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                         SGD_UINT32 uiTSResponseLength,
+                         SGD_UINT8 *pucIssuerName,
+                         SGD_UINT32 *puiIssuerNameLength, SGD_UINT8 *pucTime,
+                         SGD_UINT32 *puiTimeLength);
 
-
+/**
+ * @brief 获取时间戳的主要信息
+ * @param hTSHandle[in]: 时间戳环境句柄
+ * @param pucTSResponse[in]: 获取的时间戳响应
+ * @param uiTSResponseLength[in]: 时间戳响应的长度
+ * @param uiItemnumber[in]: 指定获取时间戳详细信息的项目编号
+ * @param pucItemValue[out]: 解析得到的时间戳相关信息
+ * @param puiItemValueLength[in,out]: 时间戳相关信息的对应长度
+ * @return 0:成功; 其他:失败
+ * @note 该函数解析时间戳的详细信息，uiItemnumber定义：
+ * STF_TIME_OF_STAMP：签发时间
+ * STF_CN_OF_TSSIGNER：签发者的通用名
+ * STF_ORINGINAL_DATA：时间戳请求的原始信息
+ * STF_CERT_OF_TSSERVER：时间戳服务器的证书
+ * STF_CERTCHAIN_OF_TSSERVER：时间戳服务器的证书链
+ * STF_SOURCE_OF_TIME：时间源的来源
+ * STF_TIME_PRECISION：时间精度
+ * STF_RESPONSE_TYPE：响应方式
+ * STF_SUBJECT_COUNTRY_OF_TSSIGNER：签发者国家
+ * STF_SUBJECT_ORGNIZATION_OF_TSSIGNER：签发者组织
+ * STF_SUBJECT_CITY_OF_TSSIGNER：签发者城市
+ * STF_SUBJECT_EMAIL_OF_TSSIGNER：签发者联系用电子信箱。
+ */
+SGD_UINT32 STF_GetTSDetail(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                           SGD_UINT32 uiTSResponseLength,
+                           SGD_UINT32 uiItemnumber, SGD_UINT8 *pucItemValue,
+                           SGD_UINT32 *puiItemValueLength);
 
 #ifdef __cplusplus
 }
--- src/stf.cpp
+++ src/stf.cpp
@@ -1,7 +1,46 @@
 #include "stf.h"
 
-SGD_UINT32 STF_InitEnvironment(void** phTSHandle){
+SGD_UINT32 STF_InitEnvironment(void **phTSHandle) { return STF_TS_OK; }
 
-    return STF_TS_OK;
+SGD_UINT32 STF_ClearEnvironment(void *hTSHandle) { return STF_TS_OK; }
+
+SGD_UINT32 STF_CreateTSRequest(void *hTSHandle, SGD_UINT8 *pucInData,
+                               SGD_UINT32 uiInDataLength, SGD_UINT32 uiReqType,
+                               SGD_UINT8 *pucTSExt, SGD_UINT32 uiHashAlgID,
+                               SGD_UINT8 *pucTSRequest,
+                               SGD_UINT32 *puiTSRequestLength) {
+  return STF_TS_OK;
 }
 
+SGD_UINT32 STF_CreateTSReponse(void *hTSHandle, SGD_UINT8 *pucTSRequest,
+                               SGD_UINT32 uiTSRequestLength,
+                               SGD_UINT32 uiSignatureAlgID,
+                               SGD_UINT8 *pucTSResponse,
+                               SGD_UINT32 *puiTSResponseLength) {
+  return STF_TS_OK;
+}
+
+SGD_UINT32 STF_VerifyTSValidity(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                                SGD_UINT32 uiTSResponseLength,
+                                SGD_UINT32 uiHashAlgID,
+                                SGD_UINT32 uiSignatureAlgID,
+                                SGD_UINT8 *pucTSCert,
+                                SGD_UINT32 uiTSCertLength) {
+  return STF_TS_OK;
+}
+
+SGD_UINT32 STF_GetTSInfo(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                         SGD_UINT32 uiTSResponseLength,
+                         SGD_UINT8 *pucIssuerName,
+                         SGD_UINT32 *puiIssuerNameLength, SGD_UINT8 *pucTime,
+                         SGD_UINT32 *puiTimeLength) {
+  return STF_TS_OK;
+}
+
+SGD_UINT32 STF_GetTSDetail(void *hTSHandle, SGD_UINT8 *pucTSResponse,
+                           SGD_UINT32 uiTSResponseLength,
+                           SGD_UINT32 uiItemnumber, SGD_UINT8 *pucItemValue,
+                           SGD_UINT32 *puiItemValueLength) {
+
+  return STF_TS_OK;
+}
